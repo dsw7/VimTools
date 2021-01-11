dae27f4a0dba0a04e813c3f06649f3292bfb784b
David Weber <dsw7@sfu.ca>
2021-01-11 07:56:55 +0000

    Add asserts

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,18 +1,23 @@
 FILENAME_ACTUAL="${TMP}/vimtools_test_actual"
 FILENAME_EXPECTED="${TMP}/vimtools_test_expected"
+LIGHT_RED="\033[1;31m"
+DARK_GREEN="\033[0;32m"
+NO_COLOR="\033[0m"
 
 assert_files_equal() {
+    local function_name=$1
+
     cmp -s $FILENAME_ACTUAL $FILENAME_EXPECTED
     local exit_code=$?
 
     if [ $exit_code == 0 ]
     then
-        echo "PASSED"
+        echo -e "${DARK_GREEN}[ PASSED ]${NO_COLOR} - $function_name"
     elif [ $exit_code == 1 ]
     then
-        echo "FAILED"
+        echo -e "${LIGHT_RED}[ FAILED ]${NO_COLOR} - $function_name"
     else
-        echo "ERROR"
+        echo -e "${LIGHT_RED}[ ERROR ]${NO_COLOR} - $function_name"
     fi
 }
 
@@ -20,7 +25,7 @@ test_sub_command_one_line() {
     echo "foo bar baz" > $FILENAME_ACTUAL
     echo "cat bar baz" > $FILENAME_EXPECTED
     vim -es -c "/foo" -c ":S cat" -c "wq" $FILENAME_ACTUAL
-    assert_files_equal
+    assert_files_equal ${FUNCNAME[0]}
 }
 
 test_sub_command_two_lines() {
@@ -33,7 +38,7 @@ cat bar baz
 cat bar baz
 EOF
     vim -es -c "/foo" -c ":S cat" -c "wq" $FILENAME_ACTUAL
-    assert_files_equal
+    assert_files_equal ${FUNCNAME[0]}
 }
 
 test_sub_command_add_limits() {
@@ -48,63 +53,9 @@ cat bar baz
 foo bar baz
 EOF
     vim -es -c "/foo" -c ":S cat 1 2" -c "wq" $FILENAME_ACTUAL
-    assert_files_equal
+    assert_files_equal ${FUNCNAME[0]}
 }
 
 test_sub_command_one_line
 test_sub_command_two_lines
 test_sub_command_add_limits
-LIGHT_PURPLE='\033[1;35m'
-LIGHT_RED='\033[1;31m'
-LIGHT_YELLOW='\033[1;33m'
-NO_COLOR='\033[0m'
-
-echo_step() {
-    echo -e "${LIGHT_PURPLE}$1${NO_COLOR}"
-}
-
-echo_error() {
-    echo -e "${LIGHT_RED}ERROR: $1${NO_COLOR}"
-}
-
-echo_warning() {
-    echo -e "${LIGHT_YELLOW}WARNING: $1${NO_COLOR}"
-}
-
-fetch_vimtools() {
-    local branch="master"
-    local repo="VimTools"
-    local archive="${repo}-${branch}.zip"
-    local inflated="${repo}-${branch}"
-    local vim_directory=".vim"
-
-    echo_step "[Step 1] - Downloading ${archive}..."
-    curl -L https://github.com/dsw7/${repo}/archive/${branch}.zip --output $archive --fail
-    if [ $? -ne 0 ]
-    then
-        echo_error "Failed to fetch VimTools!"
-        return
-    fi
-    echo
-
-    echo_step "[Step 2] - Inflating ${archive}..."
-    unzip -o ${archive}
-    echo
-
-    echo_step "[Step 3] - Remove existing $vim_directory directory..."
-    if [ -d $vim_directory ]
-    then
-        rm -rfv $vim_directory
-    else
-        echo_warning "No existing $vim_directory directory found!"
-    fi
-    echo
-
-    echo_step "[Step 4] - Rename inflated directory..."
-    mv -v $inflated $vim_directory
-    echo
-
-    echo_step "[Step 5] - Clean up any remaining files..."
-    rm -v $archive
-    echo
-}
