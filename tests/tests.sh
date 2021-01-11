d75ab7aa6b53f9427475946b7e7b4018036ea15a
David Weber <dsw7@sfu.ca>
2021-01-11 08:21:31 +0000

    Add indent test

--- tests/tests.sh
+++ tests/tests.sh
@@ -92,8 +92,24 @@ EOF
     assert_files_equal ${FUNCNAME[0]}
 }
 
+test_indent_command() {
+    cat > $FILENAME_ACTUAL << EOF
+foo bar baz
+foo bar baz
+foo bar baz
+EOF
+    cat > $FILENAME_EXPECTED << EOF
+foo bar baz
+    foo bar baz
+    foo bar baz
+EOF
+    vim -es -c ":Ind 2 3" -c "wq" $FILENAME_ACTUAL
+    assert_files_equal ${FUNCNAME[0]}
+}
+
 test_sub_command_no_limits
 test_sub_command_add_limits
 test_remove_whitespace
 test_remove_preceding_whitelines
 test_copy_command
+test_indent_command
