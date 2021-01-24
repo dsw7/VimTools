53697a9cde638df4636b84c48b3fa1ecd48ee89c
David Weber <dsw7@sfu.ca>
2021-01-24 12:32:25 +0000

    Add rm to each end

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 LOCATION_TEST_FILES="/tmp"
 FILENAME_ACTUAL="${LOCATION_TEST_FILES}/vimtools_test_actual"
-FILENAME_EXPECTED="/tmp/vimtools_test_expected"
+FILENAME_EXPECTED="${LOCATION_TEST_FILES}/vimtools_test_expected"
 LIGHT_RED="\033[1;31m"
 GREEN="\033[0;32m"
 NO_COLOR="\033[0m"
@@ -33,6 +33,8 @@ cat bar baz
 EOF
     vim -es -c "/foo" -c ":S cat" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_sub_command_one_line() {
@@ -48,6 +50,8 @@ foo bar foo
 EOF
     vim -es -c "/foo" -c ":S cat 2" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_sub_command_between_lines() {
@@ -63,6 +67,8 @@ foo bar baz
 EOF
     vim -es -c "/foo" -c ":S cat 1 2" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_remove_whitespace() {
@@ -70,6 +76,8 @@ test_remove_whitespace() {
     echo "foo bar baz" > $FILENAME_EXPECTED
     vim -es -c ":Ws" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_remove_preceding_whitelines() {
@@ -87,6 +95,8 @@ foo bar baz
 EOF
     vim -es -c ":Wl 3 4" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_copy_command() {
@@ -106,6 +116,8 @@ foo bar baz
 EOF
     vim -es -c ":Cp 1 2 3" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_indent_command() {
@@ -121,6 +133,8 @@ foo bar baz
 EOF
     vim -es -c ":Ind 2 3" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_insert_command() {
@@ -136,6 +150,8 @@ EOF
 EOF
     vim -es -c ":Ins > 1 3" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_delete_command() {
@@ -151,6 +167,8 @@ foo bar baz
 EOF
     vim -es -c ":Del 3 4" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 test_move_command() {
@@ -168,6 +186,41 @@ foo bar baz
 EOF
     vim -es -c ":Mv 1 2 3" -c "wq" $FILENAME_ACTUAL
     assert_files_equal ${FUNCNAME[0]}
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
+}
+
+test_block_command() {
+    local vimtools_block_file="${LOCATION_TEST_FILES}/vimtools_block_file"
+
+    cat > $vimtools_block_file << EOF
+def foo(x, y):
+    return x + y
+
+def bar(x, y):
+    return x - y
+EOF
+
+    cat > $FILENAME_ACTUAL << EOF
+foo bar baz
+foo bar baz
+foo bar baz
+EOF
+
+    cat > $FILENAME_EXPECTED << EOF
+foo bar baz
+foo bar baz
+def foo(x, y):
+    return x + y
+
+foo bar baz
+EOF
+
+    vim +2 -es -c ":Block $vimtools_block_file 1 2" -c "wq" $FILENAME_ACTUAL
+    assert_files_equal ${FUNCNAME[0]}
+    rm $vimtools_block_file
+    rm $FILENAME_ACTUAL
+    rm $FILENAME_EXPECTED
 }
 
 run_all_tests() {
@@ -182,7 +235,7 @@ run_all_tests() {
     test_insert_command
     test_delete_command
     test_move_command
-
+    test_block_command
     echo
     echo "Tests complete!"
     echo "Feel free to delete both of:"
