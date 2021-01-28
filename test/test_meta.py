2417a0ab4cd8cacbb5bc075f6a5b651d3abbc56d
David Weber <dsw7@sfu.ca>
2021-01-28 08:27:50 +0000

    Dedent strings

---
+++ test/test_meta.py
@@ -0,0 +1,66 @@
+import filecmp
+from os import remove
+from textwrap import dedent
+from subprocess import call
+from unittest import TestCase
+from utils.consts import (
+    FILENAME_ACTUAL,
+    FILENAME_EXPECTED,
+    TEMPORARY_COMMAND_FILE
+)
+from utils.primitives import (
+    write_executable_command_file
+)
+
+
+class TestMeta(TestCase):
+    def setUp(self):
+        input_string = """\
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        """
+
+        with open(FILENAME_ACTUAL, 'w') as f:
+            f.write(dedent(input_string))
+
+    def tearDown(self):
+        remove(FILENAME_ACTUAL)
+        remove(FILENAME_EXPECTED)
+        remove(TEMPORARY_COMMAND_FILE)
+
+    def test_false(self):
+        expected_string = """\
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        """
+
+        with open(FILENAME_EXPECTED, 'w') as f:
+            f.write(expected_string)
+
+        command = f'vim -es -c "/foo" -c ":S cat" -c "wq" {FILENAME_ACTUAL}'
+        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
+
+        call(TEMPORARY_COMMAND_FILE)
+        self.assertFalse(
+            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
+        )
+
+    def test_true(self):
+        expected_string = """\
+        cat bar baz
+        cat bar baz
+        cat bar baz
+        """
+
+        with open(FILENAME_EXPECTED, 'w') as f:
+            f.write(expected_string)
+
+        command = f'vim -es -c "/foo" -c ":S cat" -c "wq" {FILENAME_ACTUAL}'
+        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
+
+        call(TEMPORARY_COMMAND_FILE)
+        self.assertFalse(
+            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
+        )
