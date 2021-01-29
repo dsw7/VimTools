d915e8e6d54e7b23e2748b3e12d1d992c526d879
David Weber <dsw7@sfu.ca>
2021-01-29 07:59:41 +0000

    Add Mv test

--- tests/test_move.py
+++ tests/test_move.py
@@ -13,15 +13,21 @@ from utils.primitives import (
 )
 
 
-class TestWl(TestCase):
+class TestMv(TestCase):
     def setUp(self):
         input_string = """\
-        Lorem
-          ipsum
-            dolor
-              sit
-                amet...
+        namespace foo {
+            void bar() {
+                std::cout << "A foo that bars!" << std::endl;
+                std::cout << "What does that even mean?" << std::endl;
+            }
+        }
+
         """
+        # needs to be a newline after the } such that
+        # :Mv command drag line 6 (and preceding contents)
+        # to line 7
+
         with open(FILENAME_ACTUAL, 'w') as f:
             f.write(dedent(input_string))
 
@@ -30,18 +36,20 @@ class TestWl(TestCase):
         remove(FILENAME_EXPECTED)
         remove(TEMPORARY_COMMAND_FILE)
 
-    def test_remove_whitelines(self):
+    def test_move(self):
         expected_string = """\
-        Lorem
-        ipsum
-        dolor
-        sit
-        amet...
+
+        namespace foo {
+            void bar() {
+                std::cout << "A foo that bars!" << std::endl;
+                std::cout << "What does that even mean?" << std::endl;
+            }
+        }
         """
         with open(FILENAME_EXPECTED, 'w') as f:
             f.write(dedent(expected_string))
 
-        command = f'vim -es -c ":Wl 1 5" -c "wq" {FILENAME_ACTUAL}'
+        command = f'vim -es -c ":Mv 1 6 7" -c "wq" {FILENAME_ACTUAL}'
         write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
 
         call(TEMPORARY_COMMAND_FILE)
