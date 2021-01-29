80d1eec9e08dd6c7c43902b71a692b67f885abe1
David Weber <dsw7@sfu.ca>
2021-01-29 07:09:27 +0000

    Local url

--- vimtools.sh
+++ vimtools.sh
@@ -8,7 +8,7 @@ NO_COLOR="\033[0m"
 REPOSITORY_NAME="VimTools"
 FILENAME_ZIP_ARCHIVE="${REPOSITORY_NAME}-${BRANCH}.zip"
 FILENAME_INFLATED="${REPOSITORY_NAME}-${BRANCH}"
-VIMTOOLS_HOME="${PWD}/.vim"
+USER_RUNTIME_DIRECTORY="${PWD}/.vim"
 
 echo_step() {
     echo -e "${LIGHT_PURPLE}$1${NO_COLOR}"
@@ -24,7 +24,8 @@ echo_warning() {
 
 download_zip_archive() {
     echo_step "[Step 1] - Downloading ${FILENAME_ZIP_ARCHIVE}..."
-    curl -L https://github.com/dsw7/${REPOSITORY_NAME}/archive/${BRANCH}.zip --output $FILENAME_ZIP_ARCHIVE --fail
+    local url=https://github.com/dsw7/${REPOSITORY_NAME}/archive/${BRANCH}.zip 
+    curl -L $url --output $FILENAME_ZIP_ARCHIVE --fail
     if [ $? -ne 0 ]
     then
         echo_error "Failed to fetch VimTools!"
@@ -39,20 +40,20 @@ unzip_archive() {
     echo
 }
 
-remove_existing_vim_directory() {
-    echo_step "[Step 3] - Remove existing $VIMTOOLS_HOME directory..."
-    if [ -d $VIMTOOLS_HOME ]
+remove_existing_runtime_directory() {
+    echo_step "[Step 3] - Remove existing $USER_RUNTIME_DIRECTORY runtime directory..."
+    if [ -d $USER_RUNTIME_DIRECTORY ]
     then
-        rm -rfv $VIMTOOLS_HOME
+        rm -rfv $USER_RUNTIME_DIRECTORY
     else
-        echo_warning "No existing $VIMTOOLS_HOME directory found!"
+        echo_warning "No existing $USER_RUNTIME_DIRECTORY directory found!"
     fi
     echo
 }
 
 rename_inflated_directory() {
     echo_step "[Step 4] - Rename inflated directory..."
-    mv -v $FILENAME_INFLATED $VIMTOOLS_HOME
+    mv -v $FILENAME_INFLATED $USER_RUNTIME_DIRECTORY
     echo
 }
 
@@ -64,7 +65,7 @@ cleanup_files() {
 
 run_all_tests() {
     echo_step "[Step 6] - Run tests..."
-    python3 -m unittest discover --start-directory ${VIMTOOLS_HOME}/tests -v
+    python3 -m unittest discover --start-directory ${USER_RUNTIME_DIRECTORY}/tests -v
     echo
 }
 
