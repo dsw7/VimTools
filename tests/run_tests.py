dea6c4b7e4282d175790be33d7ca54f816bd4dde
David Weber <dsw7@sfu.ca>
2021-01-30 15:07:50 +0000

    Add unittest test runner

---
+++ tests/run_tests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from os import path
+from unittest import (
+    TestLoader,
+    TextTestRunner
+)
+
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
+
+def main():
+    test_directory = path.dirname(__file__)
+    test_filenames = 'test_*'
+
+    suite = TestLoader().discover(
+        test_directory, pattern=test_filenames
+    )
+    runner = TextTestRunner(verbosity=2)
+    test_result = runner.run(suite)
+
+    if test_result.wasSuccessful():
+        sys.exit(EXIT_SUCCESS)
+    else:
+        sys.exit(EXIT_FAILURE)
+
+if __name__ == "__main__":
+    main()
