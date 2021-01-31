c9fb3a89c31ddabb3b956760dfd2ad84524871cc
David Weber <dsw7@sfu.ca>
2021-01-31 07:25:25 +0000

    Fix test

---
+++ tests/test_substitute_two_digit_ranges.py
@@ -0,0 +1,82 @@
+import filecmp
+from os import remove
+from textwrap import dedent
+from subprocess import call
+from unittest import TestCase
+from utils.consts import (
+    FILENAME_ACTUAL,
+    FILENAME_EXPECTED,
+    TEMPORARY_COMMAND_FILE
+)
+from utils.primitives import (
+    write_executable_command_file
+)
+
+
+class TestSubTwoDigitRanges(TestCase):
+    def setUp(self):
+        input_string = """\
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        """
+
+        with open(FILENAME_ACTUAL, 'w') as f:
+            f.write(dedent(input_string))
+
+    def tearDown(self):
+        remove(FILENAME_ACTUAL)
+        remove(FILENAME_EXPECTED)
+        remove(TEMPORARY_COMMAND_FILE)
+
+    def test_sub_one_line(self):
+        expected_string = """\
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        cat bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        foo bar baz
+        """
+
+        with open(FILENAME_EXPECTED, 'w') as f:
+            f.write(dedent(expected_string))
+
+        command = f'vim -es -c "/foo" -c ":S cat 15" -c "wq" {FILENAME_ACTUAL}'
+        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
+
+        call(TEMPORARY_COMMAND_FILE)
+        self.assertTrue(
+            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
+        )
