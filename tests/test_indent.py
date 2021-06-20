171059730ed17b9eb989a0010fcb17fc55e9ccc9
David Weber <dsw7@sfu.ca>
2021-06-20 11:26:46 +0100

    Indent

--- tests/test_indent.py
+++ tests/test_indent.py
@@ -1,33 +1,14 @@
-import filecmp
-from os import remove
-from textwrap import dedent
-from subprocess import call
-from unittest import TestCase
-from utils.consts import (
-    FILENAME_ACTUAL,
-    FILENAME_EXPECTED,
-    TEMPORARY_COMMAND_FILE
-)
-from utils.primitives import (
-    write_executable_command_file
-)
+from helpers import VimToolsTestCase
 
 
-class TestInd(TestCase):
+class TestInd(VimToolsTestCase):
+
     def setUp(self):
-        input_string = """\
+        self.input_string = """\
         foo bar baz
         foo bar baz
         foo bar baz
         """
-
-        with open(FILENAME_ACTUAL, 'w') as f:
-            f.write(dedent(input_string))
-
-    def tearDown(self):
-        remove(FILENAME_ACTUAL)
-        remove(FILENAME_EXPECTED)
-        remove(TEMPORARY_COMMAND_FILE)
 
     def test_ind(self):
         expected_string = """\
@@ -35,16 +16,8 @@ class TestInd(TestCase):
             foo bar baz
         foo bar baz
         """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c ":Ind 1 2" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = [":Ind 1 2"]
+        self.assert_files_equal(command, self.input_string, expected_string)
 
     def test_ind_multiple_tabs(self):
         expected_string = """\
@@ -52,13 +25,5 @@ class TestInd(TestCase):
                     foo bar baz
         foo bar baz
         """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c ":Ind 1 2 3" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = [":Ind 1 2 3"]
+        self.assert_files_equal(command, self.input_string, expected_string)
