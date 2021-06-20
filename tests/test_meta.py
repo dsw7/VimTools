a4b0b9dd95bb0939ac0434ae307717c2fcd2e5c7
David Weber <dsw7@sfu.ca>
2021-06-20 07:38:28 +0100

    Update meta tests

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,33 +1,14 @@
-import filecmp
-from os import remove
-from textwrap import dedent
-from subprocess import call
-from unittest import TestCase
-from utils.consts import (
-    FILENAME_ACTUAL,
-    FILENAME_EXPECTED,
-    TEMPORARY_COMMAND_FILE
-)
-from utils.primitives import (
-    write_executable_command_file
-)
+from helpers import VimToolsTestCase
 
 
-class TestMeta(TestCase):
+class TestMeta(VimToolsTestCase):
+
     def setUp(self):
-        input_string = """\
+        self.input_string = """\
         foo bar baz
         foo bar baz
         foo bar baz
         """
-
-        with open(FILENAME_ACTUAL, 'w') as f:
-            f.write(dedent(input_string))
-
-    def tearDown(self):
-        remove(FILENAME_ACTUAL)
-        remove(FILENAME_EXPECTED)
-        remove(TEMPORARY_COMMAND_FILE)
 
     def test_false(self):
         expected_string = """\
@@ -35,17 +16,8 @@ class TestMeta(TestCase):
         foo bar baz
         foo bar baz
         """
-
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c "/foo" -c ":S cat" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertFalse(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = ["/foo", ":S cat"]
+        self.assert_files_not_equal(command, self.input_string, expected_string)
 
     def test_true(self):
         expected_string = """\
@@ -53,17 +25,8 @@ class TestMeta(TestCase):
         cat bar baz
         cat bar baz
         """
-
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c "/foo" -c ":S cat" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = ["/foo", ":S cat"]
+        self.assert_files_equal(command, self.input_string, expected_string)
 
     def test_false_indents(self):
         expected_string = """\
@@ -71,14 +34,5 @@ class TestMeta(TestCase):
         cat bar baz
             cat bar baz
         """
-
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c "/foo" -c ":S cat" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertFalse(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = ["/foo", ":S cat"]
+        self.assert_files_not_equal(command, self.input_string, expected_string)
