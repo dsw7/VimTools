5713f17c1292095ad779924513b75d6d2a4be797
David Weber <dsw7@sfu.ca>
2021-06-20 11:36:38 +0100

    Whitelines

--- tests/test_remove_whitelines.py
+++ tests/test_remove_whitelines.py
@@ -1,21 +1,10 @@
-import filecmp
-from os import remove
-from textwrap import dedent
-from subprocess import call
-from unittest import TestCase
-from utils.consts import (
-    FILENAME_ACTUAL,
-    FILENAME_EXPECTED,
-    TEMPORARY_COMMAND_FILE
-)
-from utils.primitives import (
-    write_executable_command_file
-)
+from helpers import VimToolsTestCase
 
 
-class TestWl(TestCase):
+class TestWl(VimToolsTestCase):
+
     def setUp(self):
-        input_string = """\
+        self.input_string = """\
         Lorem
           ipsum
             dolor
@@ -27,13 +16,6 @@ class TestWl(TestCase):
               sit
                 amet...
         """
-        with open(FILENAME_ACTUAL, 'w') as f:
-            f.write(dedent(input_string))
-
-    def tearDown(self):
-        remove(FILENAME_ACTUAL)
-        remove(FILENAME_EXPECTED)
-        remove(TEMPORARY_COMMAND_FILE)
 
     def test_remove_whitelines(self):
         expected_string = """\
@@ -48,16 +30,8 @@ class TestWl(TestCase):
               sit
                 amet...
         """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c ":Wl 1 5" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = [":Wl 1 5"]
+        self.assert_files_equal(command, self.input_string, expected_string)
 
     def test_remove_whitelines_two_digit_range(self):
         # This test ensures that a range 6 - 10 is not coerced to a range
@@ -75,16 +49,8 @@ class TestWl(TestCase):
         sit
         amet...
         """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c ":Wl 6 10" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = [":Wl 6 10"]
+        self.assert_files_equal(command, self.input_string, expected_string)
 
     def test_remove_whitelines_two_digit_range_backwards_range(self):
         expected_string = """\
@@ -99,13 +65,5 @@ class TestWl(TestCase):
               sit
                 amet...
         """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c ":Wl 10 6" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = [":Wl 10 6"]
+        self.assert_files_equal(command, self.input_string, expected_string)
