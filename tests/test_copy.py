cd9f8dc57bfca51479fab43cc87cbf7de3acf304
David Weber <dsw7@sfu.ca>
2021-06-20 07:33:23 +0100

    Update copy test

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -1,21 +1,10 @@
-import filecmp
-from os import remove
-from textwrap import dedent
-from subprocess import call
-from unittest import TestCase
-from utils.consts import (
-    FILENAME_ACTUAL,
-    FILENAME_EXPECTED,
-    TEMPORARY_COMMAND_FILE
-)
-from utils.primitives import (
-    write_executable_command_file
-)
+from helpers import VimToolsTestCase
 
 
-class TestCp(TestCase):
+class TestCp(VimToolsTestCase):
+
     def setUp(self):
-        input_string = """\
+        self.input_string = """\
         namespace foo {
             void bar() {
                 std::cout << "A foo that bars!" << std::endl;
@@ -26,14 +15,6 @@ class TestCp(TestCase):
         """
         # needs to be a newline after the } such that
         # :Cp command can write to line 7
-
-        with open(FILENAME_ACTUAL, 'w') as f:
-            f.write(dedent(input_string))
-
-    def tearDown(self):
-        remove(FILENAME_ACTUAL)
-        remove(FILENAME_EXPECTED)
-        remove(TEMPORARY_COMMAND_FILE)
 
     def test_copy(self):
         expected_string = """\
@@ -51,13 +32,6 @@ class TestCp(TestCase):
             }
         }
         """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
 
-        command = f'vim -es -c ":Cp 1 6 7" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = [":Cp 1 6 7"]
+        self.assert_files_equal(command, self.input_string, expected_string)
