97145ab3e6b5ad755c13da70aa8003ce06357d47
David Weber <dsw7@sfu.ca>
2021-06-20 12:15:11 +0100

    Remove primitives

--- tests/utils/primitives.py
+++
@@ -1,9 +0,0 @@
-from os import stat, chmod
-from stat import S_IEXEC
-
-def write_executable_command_file(command, filename):
-    with open(filename, 'w') as f:
-        f.write(f'#!/bin/sh\n{command}')
-
-    status_fd = stat(filename)
-    chmod(filename, status_fd.st_mode | S_IEXEC)
