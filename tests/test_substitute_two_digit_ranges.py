80c37f5548a04edf85de791de5c50b9f330269bc
David Weber <dsw7@sfu.ca>
2021-06-20 12:02:09 +0100

    Sub two digits

--- tests/test_substitute_two_digit_ranges.py
+++ tests/test_substitute_two_digit_ranges.py
@@ -1,21 +1,10 @@
-import filecmp
-from os import remove
-from textwrap import dedent
-from subprocess import call
-from unittest import TestCase
-from utils.consts import (
-    FILENAME_ACTUAL,
-    FILENAME_EXPECTED,
-    TEMPORARY_COMMAND_FILE
-)
-from utils.primitives import (
-    write_executable_command_file
-)
+from helpers import VimToolsTestCase
 
 
-class TestSubTwoDigitRanges(TestCase):
+class TestSubTwoDigitRanges(VimToolsTestCase):
+
     def setUp(self):
-        input_string = """\
+        self.input_string = """\
         foo bar baz
         foo bar baz
         foo bar baz
@@ -37,14 +26,6 @@ class TestSubTwoDigitRanges(TestCase):
         foo bar baz
         foo bar baz
         """
-
-        with open(FILENAME_ACTUAL, 'w') as f:
-            f.write(dedent(input_string))
-
-    def tearDown(self):
-        remove(FILENAME_ACTUAL)
-        remove(FILENAME_EXPECTED)
-        remove(TEMPORARY_COMMAND_FILE)
 
     def test_sub_one_line(self):
         expected_string = """\
@@ -69,17 +50,8 @@ class TestSubTwoDigitRanges(TestCase):
         foo bar baz
         foo bar baz
         """
-
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c "/foo" -c ":S cat 15" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = ["/foo", ":S cat 15"]
+        self.assert_files_equal(command, self.input_string, expected_string)
 
     def test_sub_between_lines(self):
         expected_string = """\
@@ -104,17 +76,8 @@ class TestSubTwoDigitRanges(TestCase):
         cat bar baz
         cat bar baz
         """
-
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c "/foo" -c ":S cat 16 20" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = ["/foo", ":S cat 16 20"]
+        self.assert_files_equal(command, self.input_string, expected_string)
 
     def test_sub_from_start_to_specific_line(self):
         expected_string = """\
@@ -139,17 +102,8 @@ class TestSubTwoDigitRanges(TestCase):
         foo bar baz
         foo bar baz
         """
-
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c "/foo" -c ":S cat :10" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = ["/foo", ":S cat :10"]
+        self.assert_files_equal(command, self.input_string, expected_string)
 
     def test_sub_from_specific_line_to_end_of_file(self):
         expected_string = """\
@@ -174,14 +128,5 @@ class TestSubTwoDigitRanges(TestCase):
         cat bar baz
         cat bar baz
         """
-
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c "/foo" -c ":S cat 10:" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = ["/foo", ":S cat 10:"]
+        self.assert_files_equal(command, self.input_string, expected_string)
