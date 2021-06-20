b802631954ad1bd21433069be141fdef4e274c75
David Weber <dsw7@sfu.ca>
2021-06-20 11:45:44 +0100

    Whitespace

--- tests/test_remove_whitespace.py
+++ tests/test_remove_whitespace.py
@@ -1,34 +1,16 @@
-import filecmp
-from os import remove
-from textwrap import dedent
-from subprocess import call
-from unittest import TestCase
-from utils.consts import (
-    FILENAME_ACTUAL,
-    FILENAME_EXPECTED,
-    TEMPORARY_COMMAND_FILE
-)
-from utils.primitives import (
-    write_executable_command_file
-)
+from helpers import VimToolsTestCase
 
 
-class TestWs(TestCase):
+class TestWs(VimToolsTestCase):
+
     def setUp(self):
-        input_string = [
+        self.input_string = '\n'.join([
             'Lorem  ',
             'ipsum  ',
             'dolor  ',
             'sit    ',
             'amet...'
-        ]
-        with open(FILENAME_ACTUAL, 'w') as f:
-            f.write('\n'.join(input_string))
-
-    def tearDown(self):
-        remove(FILENAME_ACTUAL)
-        remove(FILENAME_EXPECTED)
-        remove(TEMPORARY_COMMAND_FILE)
+        ])
 
     def test_remove_whitespace(self):
         expected_string = """\
@@ -38,35 +20,5 @@ class TestWs(TestCase):
         sit
         amet...
         """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c ":Ws" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        call(TEMPORARY_COMMAND_FILE)
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
-
-    def test_skip_whitespace_command_if_no_whitespace(self):
-        expected_string = """\
-        Lorem
-        ipsum
-        dolor
-        sit
-        amet...
-        """
-        with open(FILENAME_EXPECTED, 'w') as f:
-            f.write(dedent(expected_string))
-
-        command = f'vim -es -c ":Ws" -c "wq" {FILENAME_ACTUAL}'
-        write_executable_command_file(command, TEMPORARY_COMMAND_FILE)
-
-        # the second call will attempt to strip whitespace from file where none exists
-        call(TEMPORARY_COMMAND_FILE)
-        call(TEMPORARY_COMMAND_FILE)
-
-        self.assertTrue(
-            filecmp.cmp(FILENAME_ACTUAL, FILENAME_EXPECTED)
-        )
+        command = [":Ws"]
+        self.assert_files_equal(command, self.input_string, expected_string)
